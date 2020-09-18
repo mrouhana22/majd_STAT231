eaa499b60d383f9c71316f00c6eec6495fcaf42d
mrouhana22 <mrouhana22@amherst.edu>
2020-09-18 15:23:39 -0400

    uploads

--- labs/Lab04b-shiny-candyrankings/app.R
+++ labs/Lab04b-shiny-candyrankings/app.R
@@ -2,74 +2,42 @@ library(fivethirtyeight)
 library(shinythemes)
 library(tidyverse)
 
-# get "division" info (New England, Pacific, South Atlantic, etc.) for each state 
-hate_crimes2 <- hate_crimes %>%
-  left_join(fivethirtyeight::state_info, by = c("state", "state_abbrev"))
+# tidying the dataset
+candy_rankings <- candy_rankings
+candy_rankings_tidy <- candy_rankings %>%
+  gather(characteristics, present, -c(competitorname, sugarpercent, pricepercent, winpercent)) %>%
+  mutate(present = as.logical(present)) %>%
+  arrange(competitorname)
 
-# define vectors for choice values and labels 
-# can then refer to them in server as well (not just in defining widgets)
-# for selectInput, needs to be named list
-x_choices <- as.list(names(hate_crimes2)[3:11])
-x_choice_names <- c("Median household income, 2016"
-                    , "Share of the pop that in unemployed, 9/2016"
-                    , "Share of the pop that lives in metropolitan areas, 2015"
-                    , "Share of adults 25+ with a high-school degree, 2009"
-                    , "Share of the pop that are not US citizens, 2015"
-                    , "Share of white residents who are living in poverty, 2015"
-                    , "Gini index, 2015"
-                    , "Share of the pop that is not white, 2015"
-                    , "Share of 2016 US presidential votes who voted for Donald Trump")
-names(x_choices) <- x_choice_names
+# for checkboxGroupInput
+name_choices <- (candy_rankings_tidy %>%
+                  count(competitorname))$competitorname
 
-# for radio button, can be separate (have choiceValues and choiceNames options, 
-# rather than just choices)
-y_choice_values = names(hate_crimes2)[12:13]
-y_choice_names <- c("Hate crimes per 100,000 pop, SPLC, Nov 9-18,2016"
-                    , "Average annual hate crimes per 100,000 population, FBI, 2010-2015")
-
-# for checkboxGroupInput (only have "choices" option, but these labels are fine)
-div_choices <- (hate_crimes2 %>%
-                  count(division))$division
-
-# for selectInput choices, needs to be named list
-st_choices <- as.list(c("None", hate_crimes2$state_abbrev))
-names(st_choices) <- c("None", hate_crimes2$state)
+y_choice_values = names(candy_rankings_tidy)[1]
+y_choice_names <- as.list(c("None", candy_rankings_tidy$competitornames))
+names(y_choice_names) <- c("None", candy_rankings_tidy$competitornames)
 
 
 # ui 
 ui <- fluidPage(
   
-  h1("Hate Crimes in the United States"),
+  h1("Candy Power Rankings"),
+  
+  h3("Majd Rouhana"),
   
   sidebarLayout(
     sidebarPanel(
-      
-      selectInput(inputId = "x"
-                  , label = "Choose a predictor variable of interest:"
-                  , choices = x_choices
-                  , selected = "gini_index"),
-      radioButtons(inputId = "y"
-                   , label = "Choose an outcome variable of interest:"
-                   , choiceValues = y_choice_values
-                   , choiceNames = y_choice_names
-                   , selected = "hate_crimes_per_100k_splc"),
-      checkboxGroupInput(inputId = "div"
-                         , label = "Include divisions:"
-                         , choices = div_choices
-                         , selected = div_choices
+      checkboxGroupInput(inputId = "compet"
+                         , label = "Include candies:"
+                         , choices = name_choices
+                         , selected = name_choices
                          , inline = TRUE),
-      selectInput(inputId = "id_st"
-                  , label = "Identify a state in the scatterplot:"
-                  , choices = st_choices
-                  , selected = "MA")
     ),
     
     mainPanel(
-      
       tabsetPanel(type = "tabs"
-                  , tabPanel("Histogram of the outcome"
-                             , plotOutput(outputId = "hist"))
-                  , tabPanel("Scatterplot", plotOutput(outputId = "scatter"))
+                  , tabPanel("Sugar Percentile Scatterplot", plotOutput(outputId = "scatter1"))
+                  , tabPanel("Unit Price Percentile Scatterplot", plotOutput(outputId = "scatter2"))
                   , tabPanel("Table", tableOutput(outputId = "table"))
       )
     )
@@ -80,29 +48,30 @@ ui <- fluidPage(
 server <- function(input,output){
   
   use_data <- reactive({
-    data <- filter(hate_crimes2, division %in% input$div)
+    data <- filter(candy_rankings_tidy, competitorname %in% input$compet)
   })
   
-  output$hist <- renderPlot({
-    ggplot(data = use_data(), aes_string(x = input$y)) +
-      geom_histogram(color = "#00215c", fill = "#00215c", alpha = 0.7) +
-      labs(x = y_choice_names[y_choice_values == input$y]
-           , y = "Number of States")
-  })
-  
-  output$scatter <- renderPlot({
-    ggplot(data = use_data(), aes_string(x = input$x, y = input$y)) +
+  output$scatter1 <- renderPlot({
+    ggplot(data = use_data(), aes_string(x = "sugarpercent", y = "winpercent")) +
       geom_point() +
-      labs(x = names(x_choices)[x_choices == input$x]
-           , y = y_choice_names[y_choice_values == input$y]) +
-      geom_label(data = filter(hate_crimes2, state_abbrev == input$id_st)
-                 , aes(label = state_abbrev))
+      labs(x = "Sugar Percentile", y = "Win Percentage") +
+      geom_label(data = filter(candy_rankings_tidy, competitorname == input$compet)
+                 , aes(label = competitorname))
+  })
+  
+  output$scatter2 <- renderPlot({
+    ggplot(data = use_data(), aes_string(x = "pricepercent", y = "winpercent")) +
+      geom_point() +
+      labs(x = "Unit Price Percentile", y = "Win Percentage") +
+      geom_label(data = filter(candy_rankings_tidy, competitorname == input$compet)
+                 , aes(label = competitorname))
   })
   
   output$table <- renderTable({
-    dplyr::select(use_data(), state, input$x, input$y)
+    dplyr::select(use_data(), competitorname, characteristics, present, "winpercent")
   })
 }
 
 # call to shinyApp
 shinyApp(ui = ui, server = server)
+
